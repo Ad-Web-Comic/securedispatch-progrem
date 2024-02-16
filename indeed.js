1df121d25b0f59bebcee4cff2257c73a93ed9d2c
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-02-16 09:05:20 +0530

    Update indeed.js

--- indeed.js
+++ indeed.js
@@ -1,1 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const ROOT_DIR = 'C:\Users\khama\OneDrive/';
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(ROOT_DIR, req.url);
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
